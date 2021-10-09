ba42839a791986c77f2770e9a44ef4a62e2da91c
hsimao <e087754958@gmail.com>
2021-10-09 17:22:59 +0800

    edit:

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import testPlugin from './test.plugin'
+// import testPlugin from './test.plugin'
 // 全部引入
 import allPlugin from './index'
 // 單一引入
@@ -8,9 +8,7 @@ import allPlugin from './index'
 
 const app = createApp(App)
 
-app.use(allPlugin)
-
 // app.use(AppHeader as any)
 // app.use(EText as any)
 
-app.use(testPlugin).mount('#app')
+app.use(allPlugin).mount('#app')
